0a64a5dfeadcf6beec6e620bfdb1af1b0fa7fcec
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-05-23 12:08:18 -0500

    Create paginas_screen.dart

---
+++ appkedikooficial/lib/pages/paginas_screen.dart
@@ -0,0 +1,7 @@
+export 'package:flutter/material.dart';
+export 'package:appkedikooficial/pages/recetas_favoritas.dart';
+export 'package:appkedikooficial/pages/recetas_inicio.dart';
+export 'package:appkedikooficial/pages/videos_de_recetas.dart';
+
+
+//exporto los archivos dart a la pagina principal.
