96dff8720af743296ab283d6b938c5237659d7e0
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-05-23 18:50:36 -0500

    se creo el drawer

---
+++ appkedikooficial/lib/drawer/drawer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DrawerRecetas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Recetas Inicio",
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      ),
+    );
+  }
+}
