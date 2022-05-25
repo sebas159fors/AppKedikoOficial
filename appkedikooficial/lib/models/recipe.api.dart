8bafe72e7150a07f49de844ab2eeedd05d1bec3f
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-05-24 19:06:02 -0500

    Create recipe.api.dart

---
+++ appkedikooficial/lib/models/recipe.api.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:appkedikooficial/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "6f8d7fa246msh809b3948d24eeafp1fe885jsncb00aa3b9486",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
