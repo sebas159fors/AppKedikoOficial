7b87017fa81a492b1d21ecbdd9e9021390354593
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-06 16:32:47 -0500

    modificacion en el main para que arranque con firebase

--- appkedikooficial/lib/main.dart
+++ appkedikooficial/lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:appkedikooficial/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+//import 'app/my_app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MainPage());
+}
