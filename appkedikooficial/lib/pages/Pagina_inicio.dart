bd935f08a693896d72d484a2c036486bb346f950
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-13 20:47:36 -0500

    Create Pagina_inicio.dart

---
+++ appkedikooficial/lib/pages/Pagina_inicio.dart
@@ -0,0 +1,53 @@
+import 'package:appkedikooficial/src/widgets/receta_listado.dart';
+import 'package:appkedikooficial/src/widgets/slider_populares.dart';
+import 'package:appkedikooficial/src/widgets/swiper_categorias.dart';
+import 'package:appkedikooficial/src/widgets/titles.dart';
+import 'package:flutter/material.dart';
+//estilos
+import 'package:appkedikooficial/styles/styles.dart';
+//Widgets
+import '../src/widgets/app_bar.dart';
+import 'package:appkedikooficial/src/widgets/menu_lateral.dart';
+
+//se cambio de nombre a Menu Inicio para poder llamarla despues del auth
+class MenuInicio extends StatelessWidget {
+  //iniciar la porpiedad key
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: menuLateral(context),
+      key: _scaffoldKey,
+      backgroundColor: colorBG,
+      //para crear una pagina con scroll
+      body: CustomScrollView(
+        slivers: <Widget>[
+          //viene de app_bar.dart
+          // llamar a _scaffoldkey para abrir el drawer con la imagen
+          appBar(context, _scaffoldKey),
+          SliverList(
+            // a partir de este array podremos poner todos los widgets que creamos
+            delegate: SliverChildListDelegate([
+              //Column te deja poner mas widgets
+              Column(
+                children: <Widget>[
+                  swiperPopulares(),
+                  titles('Categorías', titlesStyle),
+                  sliderCategorias(),
+                  titles('Recetas Populares', titlesStyle),
+                  //para darle un poco de separacion a recetas populares
+
+                  recetasListado(context),
+                  recetasListado(context),
+                  recetasListado(context),
+                  recetasListado(context)
+                ],
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
