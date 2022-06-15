bbb673557ae4f7cda6cc94430d9b3fd84a99c689
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-15 00:56:59 -0500

    Update Pagina_inicio.dart

--- appkedikooficial/lib/pages/Pagina_inicio.dart
+++ appkedikooficial/lib/pages/Pagina_inicio.dart
@@ -1,4 +1,7 @@
-import 'package:appkedikooficial/src/widgets/receta_listado.dart';
+import 'package:appkedikooficial/src/providers/recetas_providers.dart';
+//ViewModel
+import 'package:appkedikooficial/src/viewModels/recetas_listado.dart';
+
 import 'package:appkedikooficial/src/widgets/slider_populares.dart';
 import 'package:appkedikooficial/src/widgets/swiper_categorias.dart';
 import 'package:appkedikooficial/src/widgets/titles.dart';
@@ -37,11 +40,21 @@ class MenuInicio extends StatelessWidget {
                   sliderCategorias(),
                   titles('Recetas Populares', titlesStyle),
                   //para darle un poco de separacion a recetas populares
+                  //recetasListado(context),
 
-                  recetasListado(context),
-                  recetasListado(context),
-                  recetasListado(context),
-                  recetasListado(context)
+                  //FutureBuilder se utiliza para llamar datos de una base de datos ya sea json o firebase
+                  FutureBuilder(
+                    future: recetasProvider.cargarRecetas(),
+                    initialData: [],
+                    //builder es el constructor para ordenar los datos de firebase
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<dynamic>> snapshot) {
+                      //se tiene que poner colum para que reciba los Widgets
+                      return Column(
+                        children: recetasListado(context, snapshot.data),
+                      );
+                    },
+                  ),
                 ],
               ),
             ]),
