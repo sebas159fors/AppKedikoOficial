d46e09989846c43ea2f9e9c1003bfec05a4fcac3
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-15 00:57:09 -0500

    Update menu_lateral.dart

--- appkedikooficial/lib/src/widgets/menu_lateral.dart
+++ appkedikooficial/lib/src/widgets/menu_lateral.dart
@@ -1,5 +1,42 @@
+import 'package:appkedikooficial/styles/styles.dart';
 import 'package:flutter/material.dart';
 
 Drawer menuLateral(context) {
-  return Drawer();
+  return Drawer(
+    elevation: 0.0,
+    child: Container(
+      color: colorBG,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      "https://firebasestorage.googleapis.com/v0/b/ke-diko.appspot.com/o/pizaa%20americana.jpg?alt=media&token=079ea2ac-26dc-4615-83dd-5c411b542241"),
+                  fit: BoxFit.cover),
+            ),
+            child: Container(
+              child: Text('APP KEDIKO', style: estiloTextoMenuLateral),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.fastfood, color: colorIconos),
+            title: Text('Categorías'),
+            onTap: () {
+              Navigator.pushNamed(context, 'categoria');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.help, color: colorIconos),
+            title: Text('Ayuda'),
+            onTap: () {
+              Navigator.pushNamed(context, 'categoria');
+            },
+          ),
+          //se pueden añadir mas opciones siempre especificando la ruta en routes
+        ],
+      ),
+    ),
+  );
 }
