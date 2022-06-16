7ed7197ea62ef50895b63da5baea5b95c106019e
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-15 19:45:36 -0500

    Update detalle_receta.dart

--- appkedikooficial/lib/pages/detalle_receta.dart
+++ appkedikooficial/lib/pages/detalle_receta.dart
@@ -1,40 +1,43 @@
 import 'package:appkedikooficial/src/widgets/app_bar_detalle.dart';
 import 'package:appkedikooficial/src/widgets/swiper_ingredientes.dart';
-import 'package:appkedikooficial/src/widgets/texto_receta.dart';
 import 'package:appkedikooficial/src/widgets/titles.dart';
 import 'package:appkedikooficial/styles/styles.dart';
 import 'package:flutter/material.dart';
 
 class DetallesPage extends StatelessWidget {
-  final String descripcion =
-      'Usa frutas cuyos sabores se diferencien fácilmente unos de otros. Es decir, si usas nectarinas y duraznos vas a tener sabores similares, igual que si usas mandarinas y naranjas. ¿Lo ideal? Te doy un ejemplo: fresas, mandarinas, peras y nectarinas.Otro ejemplo: sandía, kiwi y nectarinas. Puedes ir probando qué te gusta más, y claro, usando lo que tengas en la nevera.';
-
   @override
   Widget build(BuildContext context) {
+    //pasando los argumentos de receta
+    final Map<String, dynamic> receta =
+        ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       backgroundColor: colorBG,
       body: CustomScrollView(
         slivers: <Widget>[
-          appBarDetalle('Ensaladas'),
+          appBarDetalle(receta['foto']),
           SliverList(
             delegate: SliverChildListDelegate(
               [
                 Padding(
                     // Margenes de cuadro de detalles receta
                     padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
-                    child: Text('hola')
+                    child: _cuerpoReceta(receta, titlesRecipeStyleDetalle)
                     //textoReceta(titlesRecipeStyleDetalle),
                     ),
-                titles('Ingredientes', titlesStyle),
+
+                // se comento ingredientes ya que sale un error en el app que no cargan los ingredientes de firebase
+                // titles('Ingredientes', titlesStyle),
                 SizedBox(
                   height: 20.0,
                 ),
-                sliderIngredientes(),
+                //SliderIngredientes(receta['ingredientes']),
                 SizedBox(
                   height: 15.0,
                 ),
                 titles('Preparación', titlesStyle),
-                _textoDescription(descripcion)
+                //llamando a la preparacion de la receta desde la firebase
+                _textoDescription(receta['preparacion'])
               ],
             ),
           ),
@@ -42,6 +45,80 @@ class DetallesPage extends StatelessWidget {
       ),
     );
   }
+}
+
+//recibiendo receta y estilos al widget _cuerpoReceta
+Widget _cuerpoReceta(Map<String, dynamic> receta, TextStyle estilos) {
+  return Container(
+    alignment: AlignmentDirectional.topStart,
+    margin: EdgeInsets.only(left: 30.0, top: 0.0),
+    child: Column(
+      //aliniear el contenido de receta
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        //titulo de la receta
+        Text(
+          receta['titulo'],
+          textAlign: TextAlign.left,
+          style: estilos,
+        ),
+        //descropcion de la receta
+        Text(
+          receta['descripcion'],
+          textAlign: TextAlign.left,
+          style: descripcionRecipeStyle,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        //subtexto
+        Container(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.alarm, color: colorIconos),
+                    Container(
+                      margin: EdgeInsets.only(left: 5.0),
+                      child: Text(
+                        receta['duracion'],
+                        style: TextStyle(
+                            fontFamily: 'Avenir',
+                            fontWeight: FontWeight.bold,
+                            color: colorTitle,
+                            fontSize: 14.0),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.fastfood, color: colorIconos),
+                    Container(
+                      margin: EdgeInsets.only(left: 5.0),
+                      child: Text(
+                        receta['dificultad'],
+                        style: TextStyle(
+                            fontFamily: 'Avenir',
+                            fontWeight: FontWeight.bold,
+                            color: colorTitle,
+                            fontSize: 14.0),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
 }
 
 Widget _textoDescription(String texto) {
