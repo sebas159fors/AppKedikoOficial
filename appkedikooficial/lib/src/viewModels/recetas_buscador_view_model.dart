6bb21bfab03974fd74cef802a211b703fbcdbe7b
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-15 19:45:49 -0500

    Create recetas_buscador_view_model.dart

---
+++ appkedikooficial/lib/src/viewModels/recetas_buscador_view_model.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+//Estilos
+import 'package:appkedikooficial/styles/styles.dart';
+
+//recibimos los datos de provider y aqui los ordenamos
+List<Widget> recetasListadoBuscador(
+    BuildContext context, List<dynamic> recetas, String recetaBuscada) {
+  final List<Widget> listadoRecetas = [];
+
+  recetas.forEach((receta) {
+    //recibimos lo que escribe el usuario para compararlo
+    var titulo = receta['titulo'].toString().toLowerCase();
+    //creamos la condicional
+    if (titulo.contains(recetaBuscada)) {
+      final listadoWidgetProvisional =
+          _cuerpoRecetaListado(context, titlesRecipeStyle, receta);
+      listadoRecetas.add(listadoWidgetProvisional);
+    }
+  });
+
+  return listadoRecetas;
+}
+
+//el cuerpo de las recetas
+Widget _cuerpoRecetaListado(
+    BuildContext context, TextStyle estilos, Map<String, dynamic> receta) {
+  return Column(
+    children: <Widget>[
+      SizedBox(
+        height: 20.0,
+      ),
+      GestureDetector(
+        onTap: () {
+          //pasando los argumentos de receta
+          Navigator.pushNamed(context, 'detalle', arguments: receta);
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Image(
+            //aqui se pone el nombre de la clave con que has guardado la foto
+            image: NetworkImage(receta['foto']),
+            fit: BoxFit.fill,
+            width: 350.0,
+            height: 180.0,
+          ),
+        ),
+      ),
+      Container(
+        alignment: AlignmentDirectional.topStart,
+        margin: EdgeInsets.only(left: 30.0, top: 0.0),
+        child: Column(
+          //aliniear el contenido de receta
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            //titulo de la receta
+            Text(
+              receta['titulo'],
+              textAlign: TextAlign.left,
+              style: estilos,
+            ),
+            //descropcion de la receta
+            Text(
+              receta['descripcion'],
+              textAlign: TextAlign.left,
+              style: descripcionRecipeStyle,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            //subtexto
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.alarm, color: colorIconos),
+                        Container(
+                          margin: EdgeInsets.only(left: 5.0),
+                          child: Text(
+                            receta['duracion'],
+                            style: TextStyle(
+                                fontFamily: 'Avenir',
+                                fontWeight: FontWeight.bold,
+                                color: colorTitle,
+                                fontSize: 14.0),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.fastfood, color: colorIconos),
+                        Container(
+                          margin: EdgeInsets.only(left: 5.0),
+                          child: Text(
+                            receta['dificultad'],
+                            style: TextStyle(
+                                fontFamily: 'Avenir',
+                                fontWeight: FontWeight.bold,
+                                color: colorTitle,
+                                fontSize: 14.0),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ],
+  );
+}
