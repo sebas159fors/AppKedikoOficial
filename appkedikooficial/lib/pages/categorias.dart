62032a6253ca879401c37967272ec85f61502bcd
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-15 19:45:33 -0500

    Update categorias.dart

--- appkedikooficial/lib/pages/categorias.dart
+++ appkedikooficial/lib/pages/categorias.dart
@@ -1,6 +1,7 @@
+import 'package:appkedikooficial/src/providers/recetas_providers.dart';
+import 'package:appkedikooficial/src/viewModels/recetas_listado.dart';
 import 'package:appkedikooficial/src/widgets/app_bar_categorias.dart';
-import 'package:appkedikooficial/src/widgets/receta_listado.dart';
-import 'package:appkedikooficial/src/widgets/slider_populares.dart';
+
 import 'package:appkedikooficial/src/widgets/titles.dart';
 import 'package:appkedikooficial/styles/styles.dart';
 import 'package:flutter/material.dart';
@@ -8,19 +9,32 @@ import 'package:flutter/material.dart';
 class CategoriaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //aqui se recibe el objeto desde la firebase
+    final Map<String, dynamic> categoria =
+        ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       backgroundColor: colorBG,
       body: CustomScrollView(
         slivers: <Widget>[
-          appBarCategoria('Comida Vegetariana'),
+          //llamando los parametros desde la firebase
+          appBarCategoria(categoria['nombre']),
           SliverList(
             delegate: SliverChildListDelegate([
-              titles('Comida Vegetariana', titlesStyleCategoria),
-              // swiperPopulares(),
-              // recetasListadoWig(context),
-              // recetasListadoWig(context),
-              // recetasListadoWig(context),
-              // recetasListadoWig(context)
+              titles(categoria['nombre'], titlesStyleCategoria),
+              FutureBuilder(
+                future: recetasProvider
+                    .cargarRecetasCategorias(categoria['nombre']),
+                initialData: [],
+                //builder es el constructor para ordenar los datos de firebase
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<dynamic>> snapshot) {
+                  //se tiene que poner colum para que reciba los Widgets
+                  return Column(
+                    children: recetasListado(context, snapshot.data),
+                  );
+                },
+              ),
             ]),
           )
         ],
