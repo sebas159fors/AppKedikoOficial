a1bd0711d30a5769005008e11216e8ea6482532f
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-15 19:45:58 -0500

    Update swiper_categorias.dart

--- appkedikooficial/lib/src/widgets/swiper_categorias.dart
+++ appkedikooficial/lib/src/widgets/swiper_categorias.dart
@@ -1,9 +1,13 @@
+//Provider
+import 'package:appkedikooficial/src/providers/recetas_providers.dart';
+//ViewModel
+import 'package:appkedikooficial/src/viewModels/categorias_listado.dart';
 import 'package:flutter/material.dart';
 
 //Swiper
 import 'package:flutter_swiper/flutter_swiper.dart';
 
-class sliderCategorias extends StatelessWidget {
+class SliderCategorias extends StatelessWidget {
   final controller = PageController(initialPage: 1, viewportFraction: 0.3);
   @override
   Widget build(BuildContext context) {
@@ -11,14 +15,17 @@ class sliderCategorias extends StatelessWidget {
       height: 110.0,
       child: Swiper(
           itemBuilder: (BuildContext context, int index) {
-            return PageView(
-              controller: controller,
-              children: <Widget>[
-                _imagenCategoria(context),
-                _imagenCategoria(context),
-                _imagenCategoria(context),
-                _imagenCategoria(context)
-              ],
+            return FutureBuilder(
+              future: recetasProvider.cargarCategorias(),
+              initialData: [],
+              //builder es el constructor para ordenar los datos de firebase
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<dynamic>> snapshot) {
+                //se tiene que poner PageView para que reciba los Widgets
+                return PageView(
+                    controller: controller,
+                    children: categoriaListado(context));
+              },
             );
           },
           itemCount: 3,
@@ -26,6 +33,7 @@ class sliderCategorias extends StatelessWidget {
     );
   }
 
+/*
   Widget _imagenCategoria(BuildContext context) {
     return GestureDetector(
       onTap: () {
@@ -37,5 +45,5 @@ class sliderCategorias extends StatelessWidget {
             "https://firebasestorage.googleapis.com/v0/b/ke-diko.appspot.com/o/recetas%20vegetarianas%2Fpasta%20vegana.jpg?alt=media&token=5fd5b3a8-ecb4-44b6-8730-19935b1e4284"),
       )),
     );
-  }
+  }*/
 }
