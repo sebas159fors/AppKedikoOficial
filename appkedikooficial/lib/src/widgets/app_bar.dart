b511196fe1bef02bfec5579875df8082620d3866
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-15 19:45:54 -0500

    Update app_bar.dart

--- appkedikooficial/lib/src/widgets/app_bar.dart
+++ appkedikooficial/lib/src/widgets/app_bar.dart
@@ -1,3 +1,5 @@
+//delegate
+import 'package:appkedikooficial/src/delegates/search_recipes_delegate.dart';
 import 'package:flutter/material.dart';
 
 Widget appBar(BuildContext context, GlobalKey<ScaffoldState> key) {
@@ -22,13 +24,17 @@ Widget appBar(BuildContext context, GlobalKey<ScaffoldState> key) {
     actions: <Widget>[
       //icono de buscador
       Container(
-        padding: EdgeInsets.all(15.0),
-        child: Icon(
-          Icons.search,
-          color: Color.fromRGBO(183, 195, 206, 1.0),
-          size: 30.0,
-        ),
-      ),
+          padding: EdgeInsets.all(15.0),
+          child: IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Color.fromRGBO(183, 195, 206, 1.0),
+              size: 25.0,
+            ),
+            onPressed: () {
+              showSearch(context: context, delegate: RecetasSearchDelegate());
+            },
+          )),
     ],
   );
 }
