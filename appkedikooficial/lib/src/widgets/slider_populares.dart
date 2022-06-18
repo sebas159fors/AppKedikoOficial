e49c225a38be70b8fc2c0c68b2b206b7c2c2fac6
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-17 23:31:26 -0500

    Update slider_populares.dart

--- appkedikooficial/lib/src/widgets/slider_populares.dart
+++ appkedikooficial/lib/src/widgets/slider_populares.dart
@@ -16,7 +16,12 @@ class swiperPopulares extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           return PageView(
             controller: controller,
-            children: <Widget>[_tarjeta(), _tarjeta(), _tarjeta(), _tarjeta()],
+            children: <Widget>[
+              _tarjeta(),
+              _tarjeta2(),
+              _tarjeta3(),
+              _tarjeta4()
+            ],
           );
         },
         itemCount: 1,
@@ -35,6 +40,45 @@ Widget _tarjeta() {
     child: Image(
       image: NetworkImage(
           "https://firebasestorage.googleapis.com/v0/b/ke-diko.appspot.com/o/recetas%20vegetarianas%2Fbrocheta%20de%20verduras.jpg?alt=media&token=3a1c99e4-e6cf-4729-85c6-56570e820474"),
+      fit: BoxFit.fill,
+    ),
+  );
+}
+
+Widget _tarjeta2() {
+  return Container(
+    //margen del carousel de imagenes
+    margin: EdgeInsets.only(right: 30.0),
+    alignment: AlignmentDirectional.centerEnd,
+    child: Image(
+      image: NetworkImage(
+          "https://firebasestorage.googleapis.com/v0/b/ke-diko.appspot.com/o/Im%C3%A1genes%20del%20Carrusel%2Fdiseno-de-banner-de-cocina-facil.jpg?alt=media&token=764fd1ba-4923-401f-88a2-1dc281b013a0"),
+      fit: BoxFit.fill,
+    ),
+  );
+}
+
+Widget _tarjeta3() {
+  return Container(
+    //margen del carousel de imagenes
+    margin: EdgeInsets.only(right: 30.0),
+    alignment: AlignmentDirectional.centerEnd,
+    child: Image(
+      image: NetworkImage(
+          "https://firebasestorage.googleapis.com/v0/b/ke-diko.appspot.com/o/Im%C3%A1genes%20del%20Carrusel%2FPasta-con-alb%C3%B3ndigas-300x198.jpg?alt=media&token=6797aa50-08b9-488f-a99a-ff33ed1b642d"),
+      fit: BoxFit.fill,
+    ),
+  );
+}
+
+Widget _tarjeta4() {
+  return Container(
+    //margen del carousel de imagenes
+    margin: EdgeInsets.only(right: 30.0),
+    alignment: AlignmentDirectional.centerEnd,
+    child: Image(
+      image: NetworkImage(
+          "https://firebasestorage.googleapis.com/v0/b/ke-diko.appspot.com/o/Im%C3%A1genes%20del%20Carrusel%2Fensalda-de-atun.jpg?alt=media&token=3bddb6f7-c69e-4c77-9251-879230888360"),
       fit: BoxFit.fill,
     ),
   );
