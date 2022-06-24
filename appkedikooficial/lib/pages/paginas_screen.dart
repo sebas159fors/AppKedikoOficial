8497efdbc06da6936d6a68adebc0324151a9f297
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-24 17:25:02 -0500

    Delete paginas_screen.dart

--- appkedikooficial/lib/pages/paginas_screen.dart
+++
@@ -1,7 +0,0 @@
-export 'package:flutter/material.dart';
-export 'package:appkedikooficial/pages/recetas_favoritas.dart';
-export 'package:appkedikooficial/pages/recetas_inicio.dart';
-export 'package:appkedikooficial/pages/videos_de_recetas.dart';
-export 'package:appkedikooficial/drawer/drawer.dart';
-
-//exporto los archivos dart a la pagina principal.
