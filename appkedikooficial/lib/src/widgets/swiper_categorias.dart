720fd2150f2c5d0c72f38da59901ff8d2c15f5cd
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-06-24 17:25:36 -0500

    Update swiper_categorias.dart

--- appkedikooficial/lib/src/widgets/swiper_categorias.dart
+++ appkedikooficial/lib/src/widgets/swiper_categorias.dart
@@ -32,18 +32,4 @@ class SliderCategorias extends StatelessWidget {
           layout: SwiperLayout.DEFAULT),
     );
   }
-
-/*
-  Widget _imagenCategoria(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, 'categoria');
-      },
-      child: Container(
-          child: Image(
-        image: NetworkImage(
-            "https://firebasestorage.googleapis.com/v0/b/ke-diko.appspot.com/o/recetas%20vegetarianas%2Fpasta%20vegana.jpg?alt=media&token=5fd5b3a8-ecb4-44b6-8730-19935b1e4284"),
-      )),
-    );
-  }*/
 }
